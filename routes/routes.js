8acc81098ddb670ac0b214361cb6616341c04dff
Avile Sindelo <avilesindelo@gmail.com>
2023-11-21 12:33:55 +0200

    using res.json() method instead of res.send()

--- routes/routes.js
+++ routes/routes.js
@@ -22,47 +22,92 @@ export default function Routes(database){
     }
 
     async function listAllShoes(req, res){
-        //call the database method that returns all the shoes
-        let shoes = await database.getAllShoes();
+        try{
+            //call the database method that returns all the shoes
+            let shoes = await database.getAllShoes();
+            res.json({
+                status: 'success',
+                data: shoes
+            });
+        } catch(err){
+            res.json({
+                status: 'error',
+                error: err.stack
+            })
+        }
        
         //Send the returned information to the client via the Response object
-        res.send(shoes);
+    
     }
 
     async function listAllBrandShoes(req, res){
-        //Get the shoe brand from the request
-        let brandname = req.params.brandname;
-        let brand = await verifyBrand(brandname);
-      
-        //Call the database method that returns all shoes for a specific brand
-        let brandShoes = brand == `Invalid name` ? brand : await database.getBrandShoes(brand);
-        //Render a view to display all the shoes of the same brand
-        res.send(brandShoes);
+        try{
+            //Get the shoe brand from the request
+            let brandname = req.params.brandname;
+            let brand = await verifyBrand(brandname);
+          
+            //Call the database method that returns all shoes for a specific brand
+            let brandShoes = brand == `Invalid name` ? brand : await database.getBrandShoes(brand);
+            //Render a view to display all the shoes of the same brand
+            res.json({
+                status: 'success',
+                data: brandShoes
+            });
+
+        } catch(err){
+            res.json({
+                status: 'error',
+                error: err.stack
+            })
+        }
     }
 
     async function listAllSizeShoes(req, res){
-        //Get the shoes size from the request object
-        let size = req.params.size;
-        console.log(size);
-        //Call the database method that returns all the shoes of the given size
-        let sizeShoes = await database.getShoesOfSize(size);
-        //Render a view too display all the shoes of that given size 
-        res.send(sizeShoes);
+        try{
+            //Get the shoes size from the request object
+            let size = req.params.size;
+            console.log(size);
+            //Call the database method that returns all the shoes of the given size
+            let sizeShoes = await database.getShoesOfSize(size);
+            //Render a view too display all the shoes of that given size 
+            res.json({
+                status: 'success',
+                data: sizeShoes
+            })
+
+        } catch(err){
+            res.json({
+                status: 'error',
+                error: err.stack
+            });
+        }
     }
 
     async function shoesBrandAndSize(req, res){
-        //Get the brand and size from the request object
-        let brandname = req.params.brandname;
-        let size = req.params.size;
-        let brand = await verifyBrand(brandname);
-        
-        //Call the database method that returns the shoes for a given brand and size
-        let shoes = await database.getShoesSizeAndBrand(brand, size);
-        //Render a view to display the shoes
-        res.send(shoes);
+        try{
+            //Get the brand and size from the request object
+            let brandname = req.params.brandname;
+            let size = req.params.size;
+            let brand = await verifyBrand(brandname);
+            
+            //Call the database method that returns the shoes for a given brand and size
+            let shoes = await database.getShoesSizeAndBrand(brand, size);
+            //Render a view to display the shoes
+            res.json({
+                status: 'success',
+                data: shoes
+            });
+
+        } catch(err){
+            res.json({
+                status: 'error',
+                error: err.stack
+            });
+        }
     }
 
     async function updateShoeStock(req, res){
+        try{
         //Get the update information from the request - shoe id
         let shoeId = req.params.id;
         console.log(shoeId);
@@ -70,43 +115,61 @@ export default function Routes(database){
         let updateResult = await database.updateSoldShoe(shoeId); 
         console.log(updateResult)
         //Render a view that shows all the shoes  
-        res.send(updateResult);
+        res.json({
+            status: 'success',
+            data: updateResult
+        });
+        
+        } catch(err){
+            res.json({
+                status: 'error', 
+                error: err.stack
+            })
+        }
     }
 
     async function addNewShoe(req, res){
-        //Get the new shoe details from the request
-        let brand = req.body.brandName;
-        let color = req.body.color;
-        let size = req.body.size;
-        let stock = req.body.stock;
-        let price = req.body.price;
+        try{
         
-        let shoeDetails = {
-            brand: brand,
-            color: color,
-            size: size,
-            stock: stock,
-            price: price
-        
-        }
-
-        //Validate the shoe details
-    
-        if(verifyShoeDetails(shoeDetails) == true){
+            //Get the new shoe details from the request
+            let brand = req.body.brandName;
+            let color = req.body.color;
+            let size = req.body.size;
+            let stock = req.body.stock;
+            let price = req.body.price;
             
-            //Call the database and add the new shoe
-            await database.addShoeToStock(shoeDetails);
-            //image handling    
-            res.send(shoeDetails);
-        } else {
-            res.send(verifyShoeDetails(shoeDetails));
+            let shoeDetails = {
+                brand: brand,
+                color: color,
+                size: size,
+                stock: stock,
+                price: price
+            
+            }
+
+            //Validate the shoe details
+        
+            if(verifyShoeDetails(shoeDetails) == true){
+                
+                //Call the database and add the new shoe
+                await database.addShoeToStock(shoeDetails);
+                //image handling    
+                res.send(shoeDetails);
+            } else {
+                res.send(verifyShoeDetails(shoeDetails));
+            }
+        } catch(err){
+            res.json({
+                status: 'error',
+                error: err.stack
+            });
         }
 
     }
 
     function verifyShoeDetails(details){
         //Make sure the input fields are not empty
-        // if()
+       
         //Regex to test the brand name and the shoe color
         let stringRegex = /^[a-zA-Z ]+$/;
         
